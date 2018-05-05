b9ad3c90c2d32fa4655367242fe2d76336195fef
kalnynsh <kda869@yandex.ru>
2018-05-05 09:55:25 +0300

    Update code.

--- models/Product.php
+++ models/Product.php
@@ -6,6 +6,22 @@ namespace app\models;
  */
 class Product extends DataEntity
 {
+    /**
+     * Product properties
+     *
+     * @property int    $id           - product's ID
+     * @property int    $category_id  - product's category ID
+     * @property int    $image_id     - product's image ID
+     * @property int    $material_id  - product's material ID
+     * @property double $price        - product's price
+     * @property string $product_name - product's name
+     * @property string $size         - product's size
+     * @property int    $amount       - product's amount
+     * @property string $color        - product's color
+     * @property int    $status       - product's status 1|0
+     * @property \Date  $created_at   - product's creation date
+     * @property \Date  $updated_at   - product's update's date
+     */
     public $id;
     public $category_id;
     public $image_id;
