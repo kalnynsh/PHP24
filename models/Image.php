a3c4717b05f6cb91a901289de153584281ac91af
kalnynsh <kda869@yandex.ru>
2018-05-05 10:10:56 +0300

    Add doc string.

--- models/Image.php
+++ models/Image.php
@@ -6,10 +6,18 @@ namespace app\models;
  */
 class Image extends DataEntity
 {
+    /**
+     * Image properties
+     *
+     * @property int    $id           - image's ID
+     * @property string $image_name   - image name
+     * @property \Date  $created_at   - image's creation date
+     * @property \Date  $updated_at   - image's update date
+     */
     public $id;
     public $image_name;
-    public $date_created;
-    public $date_updated;
+    public $created_at;
+    public $updated_at;
 
     /**
      * Return DB table name
