64c91fa5cfdbc1cc662bc9f55324204f5b3b0e81
kalnynsh <kda869@yandex.ru>
2018-05-05 10:13:15 +0300

    Add doc string.

--- models/Category.php
+++ models/Category.php
@@ -6,6 +6,12 @@ namespace app\models;
  */
 class Category extends DataEntity
 {
+    /**
+     * Category properties
+     *
+     * @property int    $id           - category's ID
+     * @property string $product_name - category's name
+     */
     public $id;
     public $name;
 }
