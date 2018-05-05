7de87c3fa063733312735fe099037c7523a9e0eb
kalnynsh <kda869@yandex.ru>
2018-05-05 09:57:14 +0300

    Update with extends DataEntity.

--- models/Order.php
+++ models/Order.php
@@ -4,56 +4,25 @@ namespace app\models;
 /**
  *  Order model class
  */
-class Order extends DbModel
+class Order extends DataEntity
 {
-    protected $id;
-    protected $order_number;
-    protected $product_amount;
-    protected $product_id;
-    protected $user_id;
-    protected $created_at;
-
     /**
-     * Order's constructor
+     * Order's properties
      *
-     * @param int    $id             - order's ID
-     * @param string $order_number   - order's number
-     * @param string $product_amount - product's number 
-     * @param string $product_id     - product's ID 
-     * @param string $user_id        - user's ID
-     * @param string $created_at     - creation date, time
-     * @param string $updated_at     - update date, time
-     * @param string $status         - one of (
+     * @property int    $id             - order's ID
+     * @property string $order_number   - order's number
+     * @property string $product_amount - product's number 
+     * @property string $product_id     - product's ID 
+     * @property string $user_id        - user's ID
+     * @property string $created_at     - creation date, time
+     * @property string $updated_at     - update date, time
+     * @property string $status         - one of (
      *               'notactive', 'active', 'paid', 'waiting', 'delivered')
      */
-    public function __construct(
-        $id = null,
-        $order_number = null,
-        $product_amount = null,
-        $product_id = null,
-        $user_id = null,
-        $created_at = null,
-        $updated_at = null,
-        $status = null
-    ) {
-        parent::__construct();
-        $this->id = $id;
-        $this->order_number = $order_number;
-        $this->product_amount = $product_amount;
-        $this->product_id = $product_id;
-        $this->user_id = $user_id;
-        $this->created_at = $created_at;
-        $this->updated_at = $updated_at;
-        $this->status = $status;
-    }
-
-    /**
-     * Return DB table name
-     *
-     * @return string
-     */
-    public static function getTableName() : string
-    {
-        return 'orders';
-    }
+    public $id;
+    public $order_number;
+    public $product_amount;
+    public $product_id;
+    public $user_id;
+    public $created_at;
 }
