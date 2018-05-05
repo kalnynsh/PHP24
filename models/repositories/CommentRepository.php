87b307e895701d0d777fc4199edd0822261d0f65
kalnynsh <kda869@yandex.ru>
2018-05-05 09:39:01 +0300

    Create class CommentRepository.

---
+++ models/repositories/CommentRepository.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace app\models\repositories;
+
+use app\models\Comment;
+use app\models\Repository;
+
+/**
+ * Child class of Repository
+ * get DB table name,
+ * get Entity class name
+ */
+class CommentRepository extends Repository
+{
+    /**
+     * Return DB table name
+     *
+     * @return string
+     */
+    public function getTableName() : string
+    {
+        return 'comments';
+    }
+
+    /**
+     * Return Entity class name
+     *
+     * @return string
+     */
+    public function getEntityClass() : string
+    {
+        return Comment::class;
+    }
+}
