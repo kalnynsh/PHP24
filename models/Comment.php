ed6822f0f30433bc35fe9e8f637086378c1ef457
kalnynsh <kda869@yandex.ru>
2018-05-05 09:39:51 +0300

    Update code, use only props for DataEntity.

--- models/Comment.php
+++ models/Comment.php
@@ -4,37 +4,9 @@ namespace app\models;
 /**
  *  Comment model class
  */
-class Comment extends DbModel
+class Comment extends DataEntity
 {
-    protected $id;
-    protected $user_id;
-    protected $content;
-
-    /**
-     * Comment's constructor
-     *
-     * @param int    $id      - comment's ID
-     * @param int    $user_id - user's ID
-     * @param string $content - comment's content
-     */
-    public function __construct(
-        $id = null,
-        $user_id = null,
-        $content = null
-    ) {
-        parent::__construct();
-        $this->id = $id;
-        $this->user_id = $user_id;
-        $this->content = $content;
-    }
-
-    /**
-     * Return DB table name
-     *
-     * @return string
-     */
-    public static function getTableName() : string
-    {
-        return 'comments';
-    }
+    public $id;
+    public $user_id;
+    public $content;
 }
