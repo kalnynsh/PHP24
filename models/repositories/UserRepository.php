5d3f0a415dcd16db15bff8010c5cf26f66f4e9d8
kalnynsh <kda869@yandex.ru>
2018-05-05 17:15:03 +0300

    Update getUser().

--- models/repositories/UserRepository.php
+++ models/repositories/UserRepository.php
@@ -41,7 +41,7 @@ class UserRepository extends Repository
      * 
      * @return mixed - object or bool
      */
-    public function getUser($login, $pswd)
+    public function getUser(DataEntity $userEntity)
     {
         $sql = sprintf(
             "SELECT * FROM `%s` WHERE `login` = `:login`",
@@ -54,15 +54,81 @@ class UserRepository extends Repository
             $this->getEntityClass()
         );
 
-        $params = [':login' => $login];
+        $params = [':login' => $userEntity->login];
         $stmt->execute($params);
 
-        $user = $stmt->fetch();
+        $userObj = $stmt->fetch();
 
-        if (password_verify($pswd, $user->password)) {
-            return $user;
+        if (password_verify(
+            $userEntity->password,
+            $userObj->password_hash
+        )) {
+            return $userObj;
         }
 
         return false;
+    }
+
+    /**
+     * Get result of query (name) to DB by user's ID
+     *
+     * @param string $id - user's ID
+     * 
+     * @return - results of query to DB
+     */
+    function getUserById(string $id)
+    {
+        $sql = sprintf(
+            "SELECT * FROM `%s` WHERE `id` = `:id`",
+            $this->getTableName()
+        );
+        $stmt = $this->db->prepare($sql);
+        $stmt->setFetchMode(
+            \PDO::FETCH_CLASS |
+                \PDO::FETCH_PROPS_LATE,
+            $this->getEntityClass()
+        );
+
+        $params = [':id' => $id];
+        $stmt->execute($params);
+        $user = $stmt->fetch();
+
+        return $user;
+    }
+
+    /**
+     * Insert user's data to DB
+     *
+     * @param DataEntity $entity - user's DataEntity
+     * 
+     * @return - void
+     */
+    function registerUser(DataEntity $entity)
+    {
+        $params[':id'] = null;
+        $columns[] = 'id';
+        $lastlogin = date('Y-m-d H:i:s');
+
+        $userData = [
+            $entity->login,
+            $entity->name,
+            $entity->password_hash,  // password_hash($pswd, PASSWORD_DEFAULT);
+            $last_login
+        ];
+
+        foreach ($userData as $key => $value) {
+            $params[":{$key}"] = $value;
+            $columns[] = "`{$key}`";
+        }
+        $columns = implode(', ', $columns);
+        $placeholders = implode(', ', array_keys($params));
+        $tableName = $this->getTableName();
+
+        $sql = sprintf(
+            "INSERT INTO `%s` (%s) VALUES (%s)",
+            $tableName,
+            $columns,
+            $placeholders
+        );
     }
 }
