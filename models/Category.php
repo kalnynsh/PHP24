82f956828e8f094f240973bb1d6bc62e3ce6d797
kalnynsh <kda869@yandex.ru>
2018-05-05 09:34:52 +0300

    Update code, use only props for DataEntity.

--- models/Category.php
+++ models/Category.php
@@ -6,31 +6,6 @@ namespace app\models;
  */
 class Category extends DbModel
 {
-    protected $id;
-    protected $name;
-
-    /**
-     * Category's constructor
-     *
-     * @param int    $id   - category's ID
-     * @param string $name - category's name
-     */
-    public function __construct(
-        $id = null,
-        $name = null
-    ) {
-        parent::__construct();
-        $this->id = $id;
-        $this->name = $name;
-    }
-
-    /**
-     * Return DB table name
-     *
-     * @return string
-     */
-    public static function getTableName() : string
-    {
-        return 'categories';
-    }
+    public $id;
+    public $name;
 }
