8f6a60dab746b4767b9c21ab9a00f3b56ca9cd27
kalnynsh <kda869@yandex.ru>
2018-05-05 10:12:51 +0300

    Add doc string.

--- models/Material.php
+++ models/Material.php
@@ -6,6 +6,12 @@ namespace app\models;
  */
 class Material extends DataEntity
 {
+    /**
+     * Material properties
+     *
+     * @property int    $id           - material's ID
+     * @property string $material_name - material name
+     */
     public $id;
     public $material_name;
 }
