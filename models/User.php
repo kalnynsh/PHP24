8dcbfbb6b63c51bb967e59d60931882fc8756de4
kalnynsh <kda869@yandex.ru>
2018-05-05 18:41:25 +0300

    Update to public props.

--- models/User.php
+++ models/User.php
@@ -8,15 +8,17 @@ class User extends DataEntity
     /**
      * User properties
      *
-     * @property int    $id         - user's ID
-     * @property string $login      - user's login
-     * @property string $name       - user's name
-     * @property string $password   - password
-     * @property string $last_login - date of las login
+     * @property int    $id            - user's ID
+     * @property string $login         - user's login
+     * @property string $name          - user's name
+     * @property string $password_hash - password_hash($pswd, PASSWORD_DEFAULT)
+     * @property string $password      - password user's passord for check
+     * @property string $last_login    - date of last login
      */
-    protected $id;
-    protected $login;
-    protected $name;
-    protected $password;
-    protected $last_login;
+    public $id;
+    public $login;
+    public $name;
+    public $password_hash;
+    public $password;
+    public $last_login;
 }
