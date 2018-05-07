6d8b301ef241c29c3ca70db54cc6245137389566
kalnynsh <kda869@yandex.ru>
2018-05-07 10:03:55 +0300

    Add __construct().

--- models/Order.php
+++ models/Order.php
@@ -25,4 +25,37 @@ class Order extends DataEntity
     public $product_id;
     public $user_id;
     public $created_at;
+
+    /**
+     * Order's constructor
+     *
+     * @param int    $id             - order's ID
+     * @param string $order_number   - order's number
+     * @param string $product_amount - product's number 
+     * @param string $product_id     - product's ID 
+     * @param string $user_id        - user's ID
+     * @param string $created_at     - creation date, time
+     * @param string $updated_at     - update date, time
+     * @param string $status         - one of (
+     *               'notactive', 'active', 'paid', 'waiting', 'delivered')
+     */
+    public function __construct(
+        $id = null,
+        $order_number = null,
+        $product_amount = null,
+        $product_id = null,
+        $user_id = null,
+        $created_at = null,
+        $updated_at = null,
+        $status = null
+    ) {
+        $this->id = $id;
+        $this->order_number = $order_number;
+        $this->product_amount = $product_amount;
+        $this->product_id = $product_id;
+        $this->user_id = $user_id;
+        $this->created_at = $created_at;
+        $this->updated_at = $updated_at;
+        $this->status = $status;
+    }
 }
