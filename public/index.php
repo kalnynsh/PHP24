54aa3f5b67d57ae1c27fd7e056b1147644f47d10
kalnynsh <kda869@yandex.ru>
2018-05-10 12:49:13 +0300

    Refactor using App class.

--- public/index.php
+++ public/index.php
@@ -6,7 +6,7 @@
  * PHP version 7.2
  * 
  * @category Learning_PHP
- * @package  Main_Page
+ * @package  Index
  * @author   Kalnynsh <kda869@yandex.ru>
  * @license  http://example.com MIT 
  * @link     https://github.com/kalnynsh 
@@ -17,26 +17,36 @@ ini_set('display_errors', true);
 ini_set('html_errors', true);
 setlocale(LC_ALL, 'ru_RU.UTF-8', 'rus_RUS.UTF-8', 'Russian_Russia.UTF-8');
 
-require_once $_SERVER['DOCUMENT_ROOT'] . '/../config/main.php';
-require_once $_SERVER['DOCUMENT_ROOT'] . '/../config/app.php';
-require_once ROOT_DIR . '/services/Autoloader.php';
-require_once VENDOR_DIR . '/autoload.php';
+define('DS', DIRECTORY_SEPARATOR);
+use \app\base\App;
+
+require_once __DIR__ . '/../services/Autoloader.php';
+require_once __DIR__ . '/../vendor/autoloader.php';
+
+spl_autoload_register([new Autoloader(), 'loadClass']);
+
+$config = include_once __DIR__ . '/../config/main.php';
+
+App::call()->run($config);
+
+
+/*
 use app\services\Autoloader;
 use app\services\TemplateRenderer;
 use app\services\Request;
 
-spl_autoload_register([new Autoloader(), 'loadClass']);
+require_once $_SERVER['DOCUMENT_ROOT'] . '/../config/main.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/../config/app.php';
+require_once ROOT_DIR . '/services/Autoloader.php';
+require_once VENDOR_DIR . '/autoload.php';
 
 $request = new Request();
-
 $controllerName = $request->getControllerName() ? : 'product';
 $actionName = $request->getActionName();
 $id = $request->getParams('id'); // 1
-
 $controllerClass = CONTROLLERS_NAMESPACE . ucfirst($controllerName) . 'Controller';
 
-if (class_exists($controllerClass)) {
-    /** @var $controller */
+if (class_exists($controllerClass)) {   
     $controller = new $controllerClass(new TemplateRenderer());
     // Twig version
     // $controller = new $controllerClass(new TwigRenderer());
@@ -44,3 +54,5 @@ if (class_exists($controllerClass)) {
 } else {
     echo sprintf('This %s controllerClass not exists', $controllerClass);
 }
+ */
+
