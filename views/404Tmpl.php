aa97a97413f64bdc264dd082b9d11e7b38c9939a
kalnynsh <kda869@yandex.ru>
2018-05-10 08:59:25 +0300

    Update code.

--- views/404Tmpl.php
+++ views/404Tmpl.php
@@ -1,5 +1,5 @@
 <div class="products">      
-    <h3>Страница не найдена. Ошибка 404</h3>
+    <h3>Страница не найдена</h3>
     <h3>Status 404. Not Found</h3>
     <a href="/index.php">Вернуться на главную</a>  
 </div>
